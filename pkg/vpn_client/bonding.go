1a226e5360d2a641104c7c1fcf26bd41dbbddcf5
Dominik Froehlich <dominik.froehlich@sap.com>
2025-12-02 11:02:04 +0100

    switch bonding to arp_interval

--- pkg/vpn_client/bonding.go
+++ pkg/vpn_client/bonding.go
@@ -8,6 +8,7 @@ import (
 	"context"
 	"fmt"
 	"net"
+	"os/exec"
 
 	"github.com/go-logr/logr"
 	"github.com/vishvananda/netlink"
@@ -75,30 +76,31 @@ func ConfigureBonding(ctx context.Context, log logr.Logger, cfg *config.VPNClien
 		return err
 	}
 
-	tap0Link, err := netlink.LinkByName(constants.TapDevice)
+	// netlink doesn't have the options we need for IPv6 NDP validation, hence we use exec.Command here
+	// ip link add name bond0 type bond mode active-backup fail_over_mac active primary tap0 primary_reselect failure
+	//		arp_interval 1000 arp_validate filter_active arp_all_targets any arp_missed_max 2 ns_ip6_target fd8f:6d53:b97a:1::100:1,
+	//	fd8f:6d53:b97a:1::101:1
+	err = exec.Command("ip", "link", "add",
+		"name", constants.BondDevice,
+		"type", "bond",
+		"mode", "active-backup",
+		"fail_over_mac", "active",
+		"primary", "tap0",
+		"primary_reselect", "always",
+		"arp_interval", "2000",
+		"arp_validate", "active",
+		"arp_all_targets", "any",
+		"arp_missed_max", "2",
+		"ns_ip6_target", "fd8f:6d53:b97a:1::100:1,fd8f:6d53:b97a:1::101:1",
+	).Run()
+
 	if err != nil {
-		return fmt.Errorf("failed to get link %s: %w", constants.TapDevice, err)
+		return fmt.Errorf("failed to create %s link device via ip command: %w", constants.BondDevice, err)
 	}
 
-	// create bond device
-	linkAttrs := netlink.NewLinkAttrs()
-	bond := netlink.NewLinkBond(linkAttrs)
-	// use bonding
-	// - with active-backup mode
-	// - monitoring with use_carrier=1
-	// - using `primary tap0` to avoid ambiguity of selection if multiple devices are up (primary_reselect=always by default)
-	// - using `num_grat_arp 5` as safeguard on switching device
-	bond.Name = constants.BondDevice
-	bond.Mode = netlink.BOND_MODE_ACTIVE_BACKUP
-	bond.FailOverMac = netlink.BOND_FAIL_OVER_MAC_ACTIVE
-	bond.Miimon = 100
-	bond.UseCarrier = 1
-	bond.Primary = tap0Link.Attrs().Index
-	bond.NumPeerNotif = 5
-
-	log.Info("creating new bond device", "link", constants.BondDevice)
-	if err = netlink.LinkAdd(bond); err != nil {
-		return fmt.Errorf("failed to create %s link device: %w", constants.BondDevice, err)
+	bond, err := netlink.LinkByName(constants.BondDevice)
+	if err != nil {
+		return fmt.Errorf("failed to get link %s: %w", constants.BondDevice, err)
 	}
 
 	for i := range cfg.HAVPNServers {
